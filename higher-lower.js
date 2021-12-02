7886d3ad6ddf117d2d239393f201885ce5b7d93f
alexa.kearns <alexa.kearns@potatolondon.com>
2021-12-02 18:06:44 +0000

    Lint. Add button + card to Array. Doesn't render.

--- higher-lower.js
+++ higher-lower.js
@@ -1,43 +1,46 @@
 const cardDiv = document.getElementById("card-image");
-const apiPack = "https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1"
-const newCard = document.getElementById("more-button")
-let deck 
-let cards = []
+const apiPack = "https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1";
+const newCard = document.getElementById("more-button");
+let deck ;
+let cards = [];
 
 const getPack = () => {
+  anotherCard();
+
   fetch (apiPack)
   .then (res => res.json())
   .then((data) => {
-    deck = data
-    callCard()
+    deck = data;
+    callCard();
   })
   .catch((error) => {
-    console.log("Error: ", error)
-  })
-}
+    console.log("Error: ", error);
+  });
+};
 
 const callCard = () => {
   fetch(`https://deckofcardsapi.com/api/deck/${deck.deck_id}/draw/`)
   .then (res => res.json())
   .then((data) => {
-    cards.push(data.cards[0])
+    cards.push(data.cards[0]);
     createImage();
   })
   .catch((error) => {
-  })
-  cardDiv.innerHTML = cards
-}
+    console.log("Error: ", error);
+  });
+  cardDiv.innerHTML = cards;
+};
 
 const createImage = () => {
   let cardImage = document.createElement("img");
-  cardImage.src = cards[0].image
-  cardDiv.appendChild(cardImage)
-  
-}
+  cardImage.src = cards[0].image;
+  cardDiv.appendChild(cardImage);
+};
 
 const anotherCard = () => {
   newCard.addEventListener("click", function() {
-    callCard()
-  })
-}
-getPack()
+    callCard();
+  });
+};
+
+getPack();
